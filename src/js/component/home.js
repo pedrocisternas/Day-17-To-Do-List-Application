580cd8b869daf49fcda2fec930dffc0ee3ac318c
pedrocisternas <pedrocisternas86@gmail.com>
2021-05-20 00:39:16 +0000

    final

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,71 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [input, setInput] = useState("");
+	const [list, setList] = useState([]);
+	const addToList = inp => {
+		setList([...list, inp]);
+		setInput("");
+	};
+	const deleteFromList = indexToRemove => {
+		let newList = list.filter((value, i) => i != indexToRemove);
+		setList(newList);
+	};
+	const validateInput = () => {
+		if (input === "") {
+			alert("The input cannot be empty");
+			return true;
+		}
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container bg-light">
+			<h1 className="text-center">To Do List</h1>
+			<div className="input-group mb-3">
+				<input
+					type="text"
+					className="form-control"
+					placeholder="Type task"
+					aria-label="Type task"
+					aria-describedby="basic-addon2"
+					onChange={e => setInput(e.target.value)}
+					value={input}
+				/>
+				<div className="input-group-append">
+					<button
+						className="btn btn-outline-secondary"
+						type="button"
+						id="button-addon2"
+						onClick={() => {
+							if (!validateInput()) addToList(input);
+						}}>
+						Add
+					</button>
+				</div>
+			</div>
+			<div className="row">
+				<ul className="col-12">
+					{list.map((value, index) => {
+						return (
+							<>
+								<li
+									className="d-flex justify-content-between"
+									key={index}>
+									{value}
+									<span>
+										<i
+											className="fas fa-trash"
+											onClick={() =>
+												deleteFromList(index)
+											}></i>
+									</span>
+								</li>
+							</>
+						);
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 }
